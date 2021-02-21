f1fc71160b1233fedf38b9932ff8cb62d4115c29
BobbeYo1983 <880846@gmail.com>
2021-02-21 13:39:30 +0300

    Update MainActivity.java

--- app/src/main/java/ru/zizi/fortests/MainActivity.java
+++ app/src/main/java/ru/zizi/fortests/MainActivity.java
@@ -2,6 +2,8 @@ package ru.zizi.fortests;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -28,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Exit")
+                .setMessage("Are you sure you want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).setNegativeButton("No", null).show();
 
     }
 }
